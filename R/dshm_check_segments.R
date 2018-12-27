4d81c18c7414fbeb0d9c68944684b31db1da4aba
Filippo Franchini <filippo.franchini@outlook.com>
2018-12-27 12:46:59 +0000

    Plot option deleted for convenience.

--- R/dshm_check_segments.R
+++ R/dshm_check_segments.R
@@ -1,12 +1,11 @@
 #' Check if there are overlapping features between SpatialPolygons
 #'
 #' @param data Segments or other datasets containing polygons as SpatialPolygonsDataFrame.
-#' @param plot If TRUE segments are plotted with overlapping features highlighted in red. Default is FALSE.
 #' @return Nothing. It tells if there are intersecting polygons and how many are they. In case of match, it stores in the Global Environent a list 'int.ids' containing the ids of all intersecting polygons. This is used by 'dshm_correct_segments'.
 #' @author Filippo Franchini \email{filippo.franchini@@outlook.com}
 
 #' @export
-dshm_check_segments<-function(data,plot=FALSE){
+dshm_check_segments<-function(data){
 
   mat<-rgeos::gIntersects(rgeos::gBuffer(data, byid=TRUE, width=0),byid=TRUE,checkValidity=TRUE) #matrix for intersections, 'TRUE' if interaction
   int.polys<-list() #empty list to store intersections as SpatialPolygons
@@ -33,13 +32,10 @@ dshm_check_segments<-function(data,plot=FALSE){
   } else { #if there are overlapping features
     int.polys.nonulls<-int.polys[!sapply(int.polys,is.null)] #take out NULL slots from 'int.polys' list
     cat(paste(length(int.polys.nonulls)," overlapping features found. Go to 'dshm_correct_segments' function."))
-    if(plot){ #plot original dataset with segments with intersectin regions in red
-      int.polys.nonulls<-do.call(raster::bind,int.polys.nonulls)
-      raster::plot(data)
-      raster::plot(int.polys.nonulls,col="red",add=TRUE)
-    }
 
-    return(int.ids)
+    inter.Polys<-do.call(raster::bind,int.polys.nonulls)
+
+    return(list(inter.IDs = int.ids, inter.Polys = inter.Polys))
 
   }
 }
