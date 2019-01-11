6e6b9162fbaaeb274855b076aad8881a0e62ee4b
Filippo Franchini <filippo.franchini@outlook.com>
2019-01-11 18:19:57 +0000

    Modified documentation.

--- R/data.R
+++ R/data.R
@@ -1,28 +1,73 @@
 #' Observation data
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing relevant information for dolphin sightings.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 16 rows and 10 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
+#'   \item{Region.Label}{ID for stratum}
+#'   \item{Area}{Stratum area in km2}
+#'   \item{Transect.Label}{ID for transect}
+#'   \item{Sample.Label}{ID for segment}
+#'   \item{Latitude}{Segment centroid latitude}
+#'   \item{Longitude}{Segment centroid longitude}
+#'   \item{distance}{Sighting perpendicular distance from the transect line}
+#'   \item{size}{Number of dolphin}
+#'   \item{object}{Sighting ID}
+#'   \item{detected}{Required for \code{\link[Distance]{ds}} function}
 #' }
 "obsdata"
 
 
-
-
 #' Transects
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' Transect lines covered by a boad during a dolphin survey.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
+#' @format A SpatialLinesDataFrame with 15 features (i.e. lines). Associated data are not important.
 "transects"
+
+
+#' Strata
+#'
+#' Strata covered by a boad during a dolphin survey.
+#'
+#' @format A SpatialPolygonsDataFrame with 9 features (i.e. strata). Except for ID, associated data are not important.
+#' #' \describe{
+#'   \item{ID}{ID for stratum}
+#' }
+"strata"
+
+
+#' Land
+#'
+#' Land for the area of the dolphin survey.
+#'
+#' @format A SpatialPolygonsDataFrame with 30 features (i.e. islands). Associated data are not important.
+"land_crop"
+
+
+#' Distance to coast
+#'
+#' Least cost distances from the coastline for the area of the dolphin survey.
+#'
+#' @format A RasterLayer with 50 m resolution. Distances are in meters.
+"DC_crop"
+
+
+#' Distance to river
+#'
+#' Least cost distances from main rivers for the area of the dolphin survey.
+#'
+#' @format A RasterLayer with 50 m resolution. Distances are in meters.
+"DR_crop"
+
+
+#' Depth
+#'
+#' Depth for the area of the dolphin survey.
+#'
+#' @format A RasterLayer with 50 m resolution. Depth values are negative and in meters.
+"depth_crop"
+
+
+
+
