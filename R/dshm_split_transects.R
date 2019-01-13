00f50dc1fb47908f4574552c40c0e5f89e12b9d4
Filippo Franchini <filippo.franchini@outlook.com>
2019-01-13 14:28:24 +0000

    Added default starting values for inter.dist and search.time to dshm_split_transects.

--- R/dshm_split_transects.R
+++ R/dshm_split_transects.R
@@ -12,7 +12,7 @@
 #' @author Filippo Franchini \email{filippo.franchini@@outlook.com}
 
 #' @export
-dshm_split_transects<-function(transect.data,inter.dist,lwr,search.time,w,parallel=FALSE,ncores=NULL,cap = TRUE){
+dshm_split_transects<-function(transect.data,inter.dist = 0.01,lwr,search.time = 15,w,parallel=FALSE,ncores=NULL,cap = TRUE){
 
   t1<-proc.time() #starts recording time
   cat("\n\n Splitting transects...\n\n ") #message
