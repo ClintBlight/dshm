f3b880d0db680d54cd33c60e972e60181ca09dc8
Filippo Franchini <filippo.franchini@outlook.com>
2019-01-15 14:29:42 +0000

    Modified dataset documentation.

--- R/data.R
+++ R/data.R
@@ -4,14 +4,14 @@
 #'
 #' @format A data frame with 16 rows and 10 variables:
 #' \describe{
-#'   \item{Region.Label}{ID for stratum}
+#'   \item{Region.Label}{ID for stratum where the segment of the sighting is located}
 #'   \item{Area}{Stratum area in km2}
-#'   \item{Transect.Label}{ID for transect}
-#'   \item{Sample.Label}{ID for segment}
+#'   \item{Transect.Label}{ID for transect where the segment of the sighting is located}
+#'   \item{Sample.Label}{ID for segment where the sighting is located}
 #'   \item{Latitude}{Segment centroid latitude}
 #'   \item{Longitude}{Segment centroid longitude}
 #'   \item{distance}{Sighting perpendicular distance from the transect line}
-#'   \item{size}{Number of dolphin}
+#'   \item{size}{Number of dolphins}
 #'   \item{object}{Sighting ID}
 #'   \item{detected}{Required for \code{\link[Distance]{ds}} function}
 #' }
@@ -20,7 +20,7 @@
 
 #' Transects
 #'
-#' Transect lines covered by a boad during a dolphin survey.
+#' Transect lines covered by a boat during a dolphin survey.
 #'
 #' @format A SpatialLinesDataFrame with 15 features (i.e. lines). Associated data are not important.
 "transects"
@@ -28,7 +28,7 @@
 
 #' Strata
 #'
-#' Strata covered by a boad during a dolphin survey.
+#' Strata covered by a boat during a dolphin survey.
 #'
 #' @format A SpatialPolygonsDataFrame with 9 features (i.e. strata). Except for ID, associated data are not important.
 #' #' \describe{
@@ -39,7 +39,7 @@
 
 #' Land
 #'
-#' Land for the area of the dolphin survey.
+#' Land for the area of a dolphin survey.
 #'
 #' @format A SpatialPolygonsDataFrame with 30 features (i.e. islands). Associated data are not important.
 "land_crop"
@@ -47,7 +47,7 @@
 
 #' Distance to coast
 #'
-#' Least cost distances from the coastline for the area of the dolphin survey.
+#' Least cost distances from the coastline for the area of a dolphin survey.
 #'
 #' @format A RasterLayer with 50 m resolution. Distances are in meters.
 "DC_crop"
@@ -55,7 +55,7 @@
 
 #' Distance to river
 #'
-#' Least cost distances from main rivers for the area of the dolphin survey.
+#' Least cost distances from main rivers for the area of a dolphin survey.
 #'
 #' @format A RasterLayer with 50 m resolution. Distances are in meters.
 "DR_crop"
@@ -63,7 +63,7 @@
 
 #' Depth
 #'
-#' Depth for the area of the dolphin survey.
+#' Depth for the area of a dolphin survey.
 #'
 #' @format A RasterLayer with 50 m resolution. Depth values are negative and in meters.
 "depth_crop"
